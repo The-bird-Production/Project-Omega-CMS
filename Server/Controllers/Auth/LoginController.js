8507c220c65a013faeaf63c9379369e78d21ea47
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-03-27 20:59:37 +0100

    Clear code + add error code + add jwt

--- Server/Controllers/Auth/LoginController.js
+++ Server/Controllers/Auth/LoginController.js
@@ -1,34 +1,49 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient(undefined, { log: ["query"] });
-import compare from "../../Functions/compare"
 
-exports.Login = async (req,res) => {
+import compare from "../../Functions/compare";
+import jwt from "jsonwebtoken";
+const config = require("../../config/session");
 
+exports.Login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(401).json({ code: "401", message: "Missing email or password" });
+    return;
+  }
 
-    const {email, password} = req.body; 
-    if(!email || !password){
-
-        res.json(401).json({code:"401",message:"Missing email or password"})
+  try {
+    const user = await prisma.user.findFirst({ where: { email } });
+    if (!user) {
+      res
+        .status(403)
+        .json({ code: "403", message: "Invalid email or password" });
+      return;
     }
 
-    const user = await prisma.user.findFirst({where:{email}})
-    
-    if (!user) return res .status(403).send("Invalid  credentials");
+    const validPassword = await compare(user.password, password);
+    if (!validPassword) {
+      res
+        .status(403)
+        .json({ code: "403", message: "Invalid email or password" });
+      return;
+    }
 
-    const validPassword = await compare(user.password , password);
-  
-    if (!validPassword ) return res.status(403).send(' Invalid Password');
+    const token = jwt.sign({ id: user.id }, config.token_key, {
+      expiresIn: "30d",
+    });
 
-    //SEND JWT 
-
-    
-
-
-
-
-
-
-
-
-}
+    res.json(200).json({
+      code: "200",
+      message: "Success",
+      data: { user, token },
+    });
+  } catch (error) {
+    console.error("Error during authentication:", error);
+    res.status(500).json({
+      code: "500",
+      message: "Internal Server Error",
+    });
+  }
+};
