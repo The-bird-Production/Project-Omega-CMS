a82ef09e5a61b3f5b298a520058b34555f9eed59
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-03-27 21:55:45 +0100

    Add test route + connect auth route

--- Server/app.js
+++ Server/app.js
@@ -5,6 +5,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+const AuthRoute = require('./Routes/Auth/MainRoute')
+app.use('/auth',AuthRoute )
+
+app.get('/test', (req,res) => {
+    res.send("Testing Server")
+})
+
+
+console.log('App Started')
+
+
 
 
 
