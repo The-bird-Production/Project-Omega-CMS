ea988910cda5813e2d4fb580842b5348c2d98c98
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-03-28 20:07:31 +0100

    Format the code

--- Server/server.js
+++ Server/server.js
@@ -1,17 +1,17 @@
-const app = require('./app')
-const config = require('./config/server')
-require('dotenv').config()
+const app = require("./app");
+const config = require("./config/server");
+require("dotenv").config();
 
 //Security
 
-const cors = require('cors')
-const helmet = require('helmet')
-const compression = require('compression')
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
 
-app.use(helmet())
-app.use(compression())
-app.use(cors(config.CORS))
+app.use(helmet());
+app.use(compression());
+app.use(cors(config.CORS));
 
 app.listen(config.app_port, () => {
-    console.log('Backend Start on http://localhost:'+ config.app_port)
-})
+  console.log("Backend Start on http://localhost:" + config.app_port);
+});
