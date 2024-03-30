ef02a8090d162214b5b23c3b4f6dd662c7cfd500
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-03-30 14:32:45 +0100

    Formating file

--- Server/app.js
+++ Server/app.js
@@ -1,22 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const AuthRoute = require("./Routes/Auth/MainRoute");
+app.use("/auth", AuthRoute);
 
-const AuthRoute = require('./Routes/Auth/MainRoute')
-app.use('/auth',AuthRoute )
+app.get("/test", (req, res) => {
+  res.send("Testing Server");
+});
 
-app.get('/test', (req,res) => {
-    res.send("Testing Server")
-})
+console.log("App Started");
 
-
-console.log('App Started')
-
-
-
-
-
-module.exports = app
+module.exports = app;
