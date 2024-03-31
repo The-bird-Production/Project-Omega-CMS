4cb41f9c481d53ef4a915ace82b57d1cc7b24167
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-03-31 12:32:14 +0200

    Clean code + add get file and static distrubtion

--- Server/Routes/Files/MainRoute.js
+++ Server/Routes/Files/MainRoute.js
@@ -1,19 +1,21 @@
-const express = require('express')
-const router = express.Router() ; 
+const express = require("express");
+const router = express.Router();
 
 //Security MiddleWare
-const VerifyToken = require('../../Middleware/VerifyToken')
-const AuthenticateSession = require('../../Middleware/AuthenticateSession');
+const VerifyToken = require("../../Middleware/VerifyToken");
+const AuthenticateSession = require("../../Middleware/AuthenticateSession");
 
 //Controller
-const UpdateFile = require('../../Controllers/Files/UpdateFileController')
-const CreateFile = require('../../Controllers/Files/CreateFileController');
-const DeleteFIle = require('../../Controllers/Files/DeleteFileController');
+const UpdateFile = require("../../Controllers/Files/UpdateFileController");
+const CreateFile = require("../../Controllers/Files/CreateFileController");
+const DeleteFIle = require("../../Controllers/Files/DeleteFileController");
+const GetFile = require("../../Controllers/Files/GetFileController");
 
-router.post('/new', VerifyToken, AuthenticateSession, CreateFile)
-router.put('/update/:id', VerifyToken,AuthenticateSession, UpdateFile)
-router.delete('/delete/:id', VerifyToken, AuthenticateSession, DeleteFIle)
+router.post("/new", VerifyToken, AuthenticateSession, CreateFile);
+router.put("/update/:id", VerifyToken, AuthenticateSession, UpdateFile);
+router.delete("/delete/:id", VerifyToken, AuthenticateSession, DeleteFIle);
+router.get("/get/:slug", GetFile);
 
+router.use(express.static(process.cwd() + "/Public/Files/")); //Serve static files from the public folder
 
-
-module.exports = router
+module.exports = router;
