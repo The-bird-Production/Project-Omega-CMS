03d0a7c28a689a53626098516166ba7309c9441c
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-03-31 12:31:45 +0200

    Add url in config

--- Server/config/server.js
+++ Server/config/server.js
@@ -1,5 +1,7 @@
 exports.CORS = {
-    origin: "http://localhost/*",
-    allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+  origin: "http://localhost/*",
+  allowMethods: ["GET", "POST", "PUT", "DELETE"],
 };
-exports.app_port = 3001; 
+
+exports.app_port = 3001;
+exports.URL = "http://localhost" + this.app_port;
