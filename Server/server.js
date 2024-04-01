2cffca18ca501e94af9d59081043ad47fc4d9316
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-04-01 13:17:34 +0200

    Add global erreur handler

--- Server/server.js
+++ Server/server.js
@@ -20,6 +20,12 @@ app.use(compression());
 app.use(cors(config.CORS));
 app.use(urlencoded({ extended: true }));
 
+//Global  Error Handling  Middleware
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).json({ code: 500, message: "Internal server error :" + err });
+});
+
 app.listen(config.app_port, () => {
   console.log("Backend Start on http://localhost:" + config.app_port);
 });
