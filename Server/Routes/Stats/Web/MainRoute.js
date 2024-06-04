864a037b3d46ffd9714a27eb0daff63bf6ed418c
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-06-04 21:33:01 +0200

    Add New MiddleWare to support next-auth

--- Server/Routes/Stats/Web/MainRoute.js
+++ Server/Routes/Stats/Web/MainRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const VerifyToken = require("../../../Middleware/VerifyToken");
 const AuthenticateSession = require("../../../Middleware/AuthenticateSession");
 const VerifyPermissions = require("../../../Middleware/VerifyPermissions");
-
+const Auth = require("../../../Middleware/Auth");
 // Importing the controllers of this module
 const {
   GetAllStats,
@@ -12,13 +12,7 @@ const {
 } = require("../../../Controllers/Stats/Web/GetStatsController");
 const AddStats = require("../../../Controllers/Stats/Web/AddStatsController");
 
-router.get(
-  "/all",
-  VerifyToken,
-  AuthenticateSession,
-  VerifyPermissions("canViewStats"),
-  GetAllStats
-); // Getting all stats data
+router.get("/all", Auth, GetAllStats); // Getting all stats data
 router.get(
   "/date",
   VerifyToken,
