7c1218bf476b2b5603aedda13917dc78593c98ec
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-06-08 20:26:43 +0200

    Move some things to correct CORS BUG

--- Server/app.js
+++ Server/app.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const app = express();
+const config = require('./config/server')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//Security
+
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+const { urlencoded } = require("express");
+const morgan = require("morgan");
+
+if (process.env.NODE_ENV == "development") {
+  console.log("Backend running in dev mod");
+  app.use(morgan("dev"));
+} else if (process.env.NODE_ENV == "production") {
+  console.log("Backend running in production mod");
+  app.use(morgan("combined"));
+}
+app.use(helmet());
+app.use(compression());
+app.use(cors(config.CORS));
+app.use(urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 const AuthRoute = require("./Routes/Auth/MainRoute");
 const FileRoute = require("./Routes/Files/MainRoute");
--- Server/server.js
+++ Server/server.js
@@ -2,23 +2,7 @@ const app = require("./app");
 const config = require("./config/server");
 require("dotenv").config();
 
-//Security
 
-const cors = require("cors");
-const helmet = require("helmet");
-const compression = require("compression");
-const { urlencoded } = require("express");
-
-if (process.env.NODE_ENV == "development") {
-  console.log("Backend running in dev mod");
-} else if (process.env.NODE_ENV == "production") {
-  console.log("Backend running in production mod");
-}
-
-app.use(helmet());
-app.use(compression());
-app.use(cors(config.CORS));
-app.use(urlencoded({ extended: true }));
 
 //Global  Error Handling  Middleware
 app.use(function (err, req, res, next) {
