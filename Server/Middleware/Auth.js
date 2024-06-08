e5d899e235a2e8f53e19a808cd0e8ef312b0581f
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-06-08 20:28:12 +0200

    New apiAuthSystem with JWT

--- Server/Middleware/Auth.js
+++ Server/Middleware/Auth.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
+  
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Unauthorized" });
@@ -11,10 +12,10 @@ const authMiddleware = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
     next();
-    console.log(decoded);
   } catch (err) {
+    console.log(err);
+
     return res.status(401).json({ message: "Unauthorized" });
   }
 };
