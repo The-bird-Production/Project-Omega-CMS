e9288379b60806b61f29c083f5dc4b722783dc94
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-06-13 22:47:51 +0200

    Add backend in image remote paterns

--- Client/next.config.mjs
+++ Client/next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  crossOrigin:"anonymous",
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      {protocol: "http", hostname: "localhost:3001"}, 
     ],
   },
 };
