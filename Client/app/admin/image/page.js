685e48dbeb25e42702480d4d8b08e806d7a32c0e
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-07-02 15:25:42 +0200

    Ajout du breadcrumb dans images

--- Client/app/admin/image/page.js
+++ Client/app/admin/image/page.js
@@ -5,13 +5,24 @@ import { Suspense, useEffect, useState } from 'react';
 import Dashboard from '../../components/admin/Dashboard';
 import ImageList from '../../components/admin/image/ImageList';
 import AdminLayout from '../../components/layout/AdminLayout';
+import Link from 'next/link';
 
 export default function adminImage() {
   return (
     <>
       <AdminLayout>
         <Dashboard>
-          <div className='pt-5 mt-5'>
+        <nav aria-label="breadcrumb" className='text-light pt-5 mt-5'>
+              <ol className="breadcrumb">
+                <li className="breadcrumb-item">
+                  <Link href="/admin">Dashboard</Link>
+                </li>
+                <li className="breadcrumb-item active" aria-current="page">
+                  Images
+                </li>
+              </ol>
+            </nav>
+          <div className='pt-3 mt-3'>
             <Suspense
               fallback={
                 <div className="spinner-border" role="status">
