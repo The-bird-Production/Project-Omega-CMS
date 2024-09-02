afe354a708b8b848f5acfc1b93ca4ed9a9d20204
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-09-02 21:29:00 +0200

    Test pour la résolution d'un bug de permission

--- Client/Functions/CheckPermissions.js
+++ Client/Functions/CheckPermissions.js
@@ -1,30 +1,25 @@
-export default function CheckPermissions(status,session, perm) {
-
-
-
-    if (status === 'authenticated' && session.user.roleId) {
-        
-        fetch(`/api/getRole?id=${session.user.roleId}`)
-          .then((response) => response.json())
-          .then((data) => {
-            if (data.role) {
-              if (data.permissions[perm] !== true) {
-                if (data.role !== "admin") {
-                    return false
-                } else {
-                    return true 
-                }
-              }
+export default function CheckPermissions(status, session, perm) {
+  if (status === 'authenticated' && session.user.roleId) {
+     fetch(`/api/getRole?id=${session.user.roleId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.role) {
+          if (data.permissions[perm] !== true) {
+            if (data.role !== 'admin') {
+              
+              return false;
             } else {
-                return true
+              return true;
             }
-          })
-          .catch((error) => {
-            console.error('Error fetching role:', error);
-            return false
-          });
-      }
-
-
-
+            
+          }
+        } else {
+          return true;
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching role:', error);
+        return error;
+      });
+  }
 }
