c9cff92c646d10b5001b7d67abf0122c3f7dae3c
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-09-02 21:27:02 +0200

    Correction d'un bug de format

--- Server/Controllers/Role/DeleteRoleController.js
+++ Server/Controllers/Role/DeleteRoleController.js
@@ -4,16 +4,19 @@ const prisma = new PrismaClient();
 
 const DeleteRole = async (req, res) => {
   try {
+    const int = parseInt(req.params.id)
     await prisma.role.delete({
       where: {
-        id: req.params.id,
+        id: int,
       },
     });
     res.status(200).json({ code: 200, message: "Role deleted sucessfully" });
   } catch (error) {
+    console.log(error)
     return res
       .status(200)
-      .json({ code: 200, message: "Internal Server Error " + e });
+      .json({ code: 200, message: "Internal Server Error " + error });
+    
   }
 };
 
