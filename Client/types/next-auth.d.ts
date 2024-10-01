10ab5add0106c20a70790c4f36291c2182100fdc
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-10-01 21:08:40 +0200

    Add permissions in next auth session object type

--- Client/types/next-auth.d.ts
+++ Client/types/next-auth.d.ts
@@ -1,9 +1,12 @@
 import { DefaultSession } from "next-auth";
+import { JsonValue } from "next-auth/adapters";
 
 
 declare module "next-auth" {
     interface Session {
         user: User & DefaultSession["user"]
+        permissions : object | JsonValue | undefined
+
     }
 
     interface User {
