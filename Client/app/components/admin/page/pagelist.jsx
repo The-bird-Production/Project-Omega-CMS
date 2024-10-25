36310c7f05a7b5e5c7db3b27017514700a00bed2
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-10-25 14:06:52 +0200

    Create pageListe component

---
+++ Client/app/components/admin/page/pagelist.jsx
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function PageList() {
+  const { data: session, status } = useSession({
+    required: true,
+  });
+
+  const [rowData, setRowData] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (status === 'authenticated') {
+        const token = session.accessToken || session.user.accessToken;
+        try {
+          const res = await fetch(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/page/get/all`,
+            {
+              headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+              },
+              mode: 'cors',
+            }
+          );
+
+          if (res.ok) {
+            const data = await res.json();
+            const jsonData = data.data;
+
+            setRowData(jsonData);
+          } else {
+            console.error('Failed to fetch data:', res.statusText);
+          }
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      }
+    };
+
+    fetchData();
+
+    const intervalId = setInterval(fetchData, 15000);
+
+    return () => clearInterval(intervalId);
+  }, [session, status]);
+
+  const editPage = (id) => {
+    router.push(`/admin/page/edit/${id}`);
+  }
+
+  const delPage = async (id) => {
+    if (status === 'authenticated') {
+      const token = session.accessToken || session.user.accessToken;
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/page/delete/${id}`,
+          {
+            method: 'DELETE',
+            headers: {
+              'Authorization': `Bearer ${token}`,
+              'Content-Type': 'application/json',
+            },
+            mode: 'cors',
+          }
+        );
+
+        if (res.ok) {
+          setRowData((prevData) => prevData.filter((item) => item.id !== id));
+        } else {
+          console.error('Failed to delete image:', res.statusText);
+        }
+      } catch (error) {
+        console.error('Error deleting image:', error);
+      } 
+    }
+  }
+
+  return rowData.map((item, index) => (
+    <div className="card card-body m-3 bg-primary text-white" key={index}>
+      <div className='container row'>
+        <div className="col-10">{item.title}</div>
+        <div className="col-2">
+          <button className='btn btn-secondary m-1' onClick={() => editPage(item.id)}><i className="bi bi-pencil-square"></i></button>
+          <button className='btn btn-secondary' onClick={() => delPage(item.id)}><i className="bi bi-trash"></i></button>
+        </div>
+      </div>
+    </div>
+  ));
+}
