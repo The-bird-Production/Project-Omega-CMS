c9fef8b128f32c6e9c9d875d45856429af79dde8
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-12-04 22:59:43 +0100

    background color bug correction

--- Client/app/components/layout/AdminLayout.jsx
+++ Client/app/components/layout/AdminLayout.jsx
@@ -1,25 +1,25 @@
-"use client";
+'use client';
 
-import Head from "next/head";
-import "../../../public/css/admin.css";
-import "../../../public/js/chart";
+import Head from 'next/head';
+import '../../../public/css/admin.css';
+import '../../../public/js/chart';
 
-import Link from "next/link";
-import Script from "next/script";
-import { useEffect } from "react";
+import Link from 'next/link';
+import Script from 'next/script';
+import { useEffect } from 'react';
 
 function Layout({ children }) {
   useEffect(() => {
-    require("../../../public/js/bootstrap.bundle.min.js");
-    require("../../../public/js/chart");
+    require('../../../public/js/bootstrap.bundle.min.js');
+    require('../../../public/js/chart');
   });
   return (
     <>
-      
-        <div className=" vh-100 bg-primary text-light">
-        <div className="container">{children}</div>
+      <body className='bg-primary'>
+        <div className="text-light">
+          <div className="container">{children}</div>
         </div>
-      
+      </body>
     </>
   );
 }
