2dcf132fa5ffb3628690129dd12e50fb48a730fd
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-12-10 21:35:07 +0100

    Début de la création pour la modification des users

--- Client/app/admin/user/edit/[id]/page.jsx
+++ Client/app/admin/user/edit/[id]/page.jsx
@@ -1,9 +1,12 @@
-"use client"
+'use client';
 
 import { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import AdminLayout from '../../../../components/layout/AdminLayout';
 import Dashboard from '../../../../components/admin/Dashboard';
+import Link from 'next/link';
+import Image from 'next/image';
+import FormatedDate from '../../../../components/util/FormatedDate';
 
 export default function Page({ params }) {
   const { data: session, status } = useSession();
@@ -42,21 +45,66 @@ export default function Page({ params }) {
       }
     };
 
-    
-      if (!userData) {
-        fetchData()
-      }
-    
-
-    
-  });
+    fetchData();
+  }, [session, status]);
 
   return (
     <>
       <AdminLayout>
         <Dashboard>
-            {userData.id}
+          <nav aria-label="breadcrumb" className="text-light pt-5 mt-5">
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item">
+                <Link href="/admin">Dashboard</Link>
+              </li>
+              <li className="breadcrumb-item" aria-current="page">
+                <Link href="/admin/user">Utilisateurs</Link>
+              </li>
+              <li className="breadcrumb-item active" aria-current="page">
+                {userData.id}
+              </li>
+            </ol>
+          </nav>
+          <div className="pt-3 mt-3">
+            <div className="card">
+              <div className="card-body bg-secondary rounded border border-secondary">
+                <h2 className="card-title text-light">
+                  Modification d'utilisateur :{' '}
+                </h2>
+                <form>
+                  <div className="text-white container row pt-3 ">
+                    <div className="col-6">
+                      <h2>{userData.name || userData.username}</h2>
+                    </div>
 
+                    <div className="col-6">
+                      <h4>Profil picture : </h4>
+                      <Image
+                        src={userData.image}
+                        width="125"
+                        height="125"
+                        className="img-fluid"
+                      ></Image>
+                    </div>
+                    <div className="col-6">
+                      <h3>Informations : </h3>
+                      <h4>Email : <input className='form-control' type='text' value={userData.email}></input> {userData.email}</h4>
+                      <h4>Email verified : {userData.emailVerified}</h4>
+                      <h4>Role : {userData.roleId} </h4>
+                      <h5>
+                        Creation date :{' '}
+                        <FormatedDate rowDate={userData.createdAt} />
+                      </h5>
+                      <h5>
+                        Last update :{' '}
+                        <FormatedDate rowDate={userData.updatedAt} />
+                      </h5>
+                    </div>
+                  </div>
+                </form>{' '}
+              </div>
+            </div>
+          </div>
         </Dashboard>
       </AdminLayout>
     </>
