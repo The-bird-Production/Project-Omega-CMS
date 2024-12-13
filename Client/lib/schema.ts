7321c10e960ffe97c69f1b219dba41e27eb8e42b
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-12-14 00:24:33 +0100

    Correct schema due to bug

--- Client/lib/schema.ts
+++ Client/lib/schema.ts
@@ -19,7 +19,10 @@ export const pageSchema = z.object({
 
 export const userSchema = z.object({
     username: z.string().min(3, "Username is required").max(10, "Lenght is to mutch").nullable().optional(),
-    email: z.string().min(5, "Email is required").email("Must be an email").nullable().optional(),
-    role : z.number(), 
+    email: z.string()
+    .email("Must be a valid email")
+    .or(z.literal(null)) 
+    .optional(), 
+    roleId : z.number(), 
     emailVerified: z.boolean().nullable().optional()
 })
