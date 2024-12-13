f838ccd6413bc485e771dbd39a9b70a0f0c7a11c
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-12-14 00:22:26 +0100

    Correct permission Reading bug

--- Server/Middleware/VerifyPermissions.js
+++ Server/Middleware/VerifyPermissions.js
@@ -7,14 +7,15 @@ const VerifyPermission = (permission) => {
       const authHeader = req.headers.authorization;
       const token = authHeader.split(" ")[1];
       const decode = jwt.decode(token);
-      const { permissions } = await GetPermissionByRoleId(decode.roleId);
-
-      if (permissions[permission] || permissions["admin"]) {
+      const { permissions } = await GetPermissionByRoleId(decode.user.roleId);
+      
+      if (permissions["admin"] || permissions[permission]) {
         next();
       } else {
         res.status(403).json({ code: 403, message: "Forbidden " });
       }
     } catch (e) {
+      console.log(e)
       return res
         .status(500)
         .json({ code: 500, message: "Internal Server Error: " + e });
