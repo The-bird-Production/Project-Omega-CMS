a9aecdf94c35591d62b2598c7c17dfca7dc50a89
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-12-19 18:17:47 +0100

    Add card style in image list

--- Client/app/admin/image/page.js
+++ Client/app/admin/image/page.js
@@ -12,26 +12,29 @@ export default function adminImage() {
     <>
       <AdminLayout>
         <Dashboard>
-        <nav aria-label="breadcrumb" className='text-light pt-5 mt-5'>
-              <ol className="breadcrumb">
-                <li className="breadcrumb-item">
-                  <Link href="/admin">Dashboard</Link>
-                </li>
-                <li className="breadcrumb-item active" aria-current="page">
-                  Images
-                </li>
-              </ol>
-            </nav>
-          <div className='pt-3 mt-3'>
-            <Suspense
-              fallback={
-                <div className="spinner-border" role="status">
-                  <span className="sr-only"></span>
-                </div>
-              }
-            >
-              <ImageList></ImageList>
-            </Suspense>
+          <nav aria-label="breadcrumb" className="text-light pt-5 mt-5">
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item">
+                <Link href="/admin">Dashboard</Link>
+              </li>
+              <li className="breadcrumb-item active" aria-current="page">
+                Images
+              </li>
+            </ol>
+          </nav>
+          <div className="pt-3 mt-3">
+            <div className='card card-body bg-secondary'>
+              <Suspense
+                fallback={
+                  <div className="spinner-border" role="status">
+                    <span className="sr-only"></span>
+                  </div>
+                }
+              >
+                <ImageList></ImageList>
+              </Suspense>
+              <div className='mt-2'><Link href="/admin/image/new" className='btn btn-primary'><i class="bi bi-plus-circle"></i> New</Link></div>
+            </div>
           </div>
         </Dashboard>
       </AdminLayout>
