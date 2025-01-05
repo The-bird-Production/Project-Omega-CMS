90d47a1afc0467a87d43e3fa34dca38288c83f84
Faynix <59486270+Faynix-code@users.noreply.github.com>
2025-01-05 13:38:21 +0100

    Add example plugin

---
+++ Server/Plugins/example-plugin/admin/dashboard.js
@@ -0,0 +1,10 @@
+export default function Dashboard () {
+
+    return (
+        <>
+        <h1>Admin Plugin Dashboard component</h1>
+        </>
+    )
+
+
+}
---
+++ Server/Plugins/example-plugin/public/publicComponent.js
@@ -0,0 +1,10 @@
+
+
+export default function Component () {
+    return (
+        <>
+        <h1>Puglic view of Plugin</h1>
+        </>
+    )
+
+}
