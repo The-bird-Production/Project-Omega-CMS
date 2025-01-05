eb381d1939d601c829a0295a99b9bf1b9207e4aa
Faynix <59486270+Faynix-code@users.noreply.github.com>
2025-01-05 16:01:42 +0100

    Add example plugin dashboard

---
+++ Client/app/components/plugin/example-plugin/dashboard.js
@@ -0,0 +1,10 @@
+"use client"
+
+export default function Dashboard() {
+  return (
+    <div className="mt-5 card card-body bg-secondary">
+      <h1>Example Plugin</h1>
+      <p>Dashboard</p>
+    </div>
+  );
+}
