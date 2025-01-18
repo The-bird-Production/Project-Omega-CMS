2d03780f3684f903ac5fdd1e70310dc5dcd3a7c1
Faynix <59486270+Faynix-code@users.noreply.github.com>
2025-01-18 20:26:15 +0100

    Add login for plugin install

--- Server/Routes/Plugins/MainRoute.js
+++ Server/Routes/Plugins/MainRoute.js
@@ -4,10 +4,11 @@ const router = express.Router()
 const Auth = require("../../Middleware/Auth");
 const VerifyPermissions = require("../../Middleware/VerifyPermissions");
 const PluginsController = require('../../Controllers/Plugins/PluginsController')
+const AddLogs = require('../../Functions/AddLogs')
 
 router.get("/", Auth, VerifyPermissions("canManagePlugins"), PluginsController.getPluginsInstalled)
 router.get("/installable", Auth, VerifyPermissions("canManagePlugins"), PluginsController.getInstallablePlugins)
-router.post("/install/:id", Auth, VerifyPermissions("canManagePlugins"), PluginsController.InstallPlugin)
+router.post("/install/:id", Auth, VerifyPermissions("canManagePlugins"),AddLogs("Install a new plugin" , "green"), PluginsController.InstallPlugin)
 router.get("/user-routes", PluginsController.getUserRoutes)
 
 
