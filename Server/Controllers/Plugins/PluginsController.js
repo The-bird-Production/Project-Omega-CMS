b5e5319be8cc0979d5dfac5fe7543b40fce35042
Faynix <59486270+Faynix-code@users.noreply.github.com>
2025-01-18 20:15:53 +0100

    Some change more in desc
    -Add id support
    -Add get installable plugin from marketplace
    -Create install route manager

--- Server/Controllers/Plugins/PluginsController.js
+++ Server/Controllers/Plugins/PluginsController.js
@@ -1,19 +1,46 @@
 const fs = require("fs");
 const path = require("path");
+const InstallPlugins = require("../../Functions/InstallPlugins");
 
-exports.getPlugins = (req, res) => {
+exports.getPluginsInstalled = (req, res) => {
   const pluginsDir = path.resolve(process.cwd(), "Plugins");
   const plugins = fs.readdirSync(pluginsDir).map((plugin) => {
     const manifest = require(path.join(pluginsDir, plugin, "plugin.json"));
     return {
       name: manifest.name,
+      id: manifest.id,
       version: manifest.version,
-      description : manifest.description,
-      folder: manifest.folder
+      description: manifest.description,
+      folder: manifest.folder,
     };
   });
   res.json(plugins);
 };
+
+exports.getInstallablePlugins = async (req, res) => {
+  const response = await fetch(
+    "https://omega.marketplace.thebirdproduction.fr/plugins"
+  );
+  const plugins = await response.json();
+
+  res.json(plugins);
+};
+
+exports.InstallPlugin = async (req,res) => {
+  const pluginId = req.params.id;
+
+  try {
+    await InstallPlugins(pluginId);
+  } catch(err) {
+    console.error(err);
+    res.status(500).json({error: "Erreur lors de l'installation du plugin"});
+  }
+
+  
+  
+  
+
+}
 
 exports.getUserRoutes = (req, res) => {
   const pluginsDir = path.resolve(process.cwd(), "Plugins");
