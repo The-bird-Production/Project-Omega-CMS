3442032e5fdc2a8fcffc8d1f1751fb298ab4cf78
Faynix <59486270+Faynix-code@users.noreply.github.com>
2025-01-18 17:05:33 +0100

    Add breadcrumb to plugins list page

--- Client/app/admin/plugins/page.jsx
+++ Client/app/admin/plugins/page.jsx
@@ -1,18 +1,32 @@
 'use client';
 import dynamic from 'next/dynamic';
 import Dashboard from '../../components/admin/Dashboard';
+import Link from 'next/link';
 
-
-const AdminLayout = dynamic(() => import('../../components/layout/AdminLayout'), {
-  ssr: false,
-});
-import PluginPage from "../../components/plugin/list";
+const AdminLayout = dynamic(
+  () => import('../../components/layout/AdminLayout'),
+  {
+    ssr: false,
+  }
+);
+import PluginPage from '../../components/plugin/list';
 
 export default function Admin() {
   return (
     <>
       <AdminLayout>
         <Dashboard>
+          <nav aria-label="breadcrumb" className="text-light pt-5 mt-5">
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item">
+                <Link href="/admin">Dashboard</Link>
+              </li>
+              <li className="breadcrumb-item active" aria-current="page">
+                Plugins
+              </li>
+            </ol>
+          </nav>
+
           <PluginPage></PluginPage>
         </Dashboard>
       </AdminLayout>
