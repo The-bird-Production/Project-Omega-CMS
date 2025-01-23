4ab7393a22dea70bc11794b3fe311b2529aaf6ca
Faynix <59486270+Faynix-code@users.noreply.github.com>
2025-01-23 18:19:39 +0100

    Add database migration system for plugins

--- Server/Functions/InstallPlugins.js
+++ Server/Functions/InstallPlugins.js
@@ -3,6 +3,11 @@ const path = require("path");
 const AdmZip = require("adm-zip");
 const axios = require("axios");
 const cms = require('../../cms');
+const { exec } = require('child_process');
+const url = require('url');
+
+
+
 
 const InstallPlugins = async (pluginId) => {
   try {
@@ -43,8 +48,50 @@ const InstallPlugins = async (pluginId) => {
     fs.renameSync(pluginDir + "/public/publicComponent.js", clientDir + `/${pluginId}/publicComponent.js`);
     fs.rmSync(pluginDir + "/admin/");
     fs.rmSync(pluginDir + "/public/");
+    
+    //Database migration 
 
 
+    //Backup the database
+    const backupTimestamp = new Date().toISOString().replace(/[:.]/g, '-');
+    const backupFileName = `backup_${backupTimestamp}.sql`;
+    const backupFilePath = path.join(BACKUP_DIR, backupFileName);
+
+    const DATABASE_URL = process.env.DATABASE_URL || 'mysql://user:password@localhost:3306/db_name';
+    const { hostname, port, pathname, auth } = new url.URL(DATABASE_URL);
+    const [user, password] = auth.split(':');
+    const dbName = pathname.replace('/', ''); 
+    const dumpCommand = `mysqldump -u ${user} -p${password} -h ${hostname} -P ${port || 3306} ${dbName} > ${backupFilePath}`;
+
+    exec(dumpCommand, (error, stdout, stderr) => {
+      if (error) {
+        console.error('Erreur lors de la sauvegarde :', error.message);
+        return;
+      }
+      if (stderr) {
+        console.error('Messages d’erreur :', stderr);
+        return;
+      }
+      console.log(`Sauvegarde réussie : ${backupFilePath}`);
+    });
+
+    //Apply the migration
+    const migrationPath = path.join(pluginDir, "prisma", 'migration.sql');
+
+    if (!fs.existsSync(migrationPath)) {
+      return new Error("Aucun fichier de migration trouvé.");
+    }
+
+    const migrationSQL = fs.readFileSync(migrationPath, 'utf-8');
+
+    
+    if (!migrationSQL.trim()) {
+      return new Error("Fichier de migration vide.");
+    }
+
+    await prisma.$executeRawUnsafe(migrationSQL);
+    
+   
   } catch (err) {
     console.error("Erreur lors de l'installation du plugin :", err);
     return err;
