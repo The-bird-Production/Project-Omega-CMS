bca21a07dbed3149b10be9edb7dc8eb9c681dda8
Faynix <59486270+Faynix-code@users.noreply.github.com>
2025-01-28 20:41:29 +0100

    Add loadAllPlugin Function and the controller for install plugins

--- Server/Routes/Plugins/MainRoute.js
+++ Server/Routes/Plugins/MainRoute.js
@@ -5,11 +5,15 @@ const Auth = require("../../Middleware/Auth");
 const VerifyPermissions = require("../../Middleware/VerifyPermissions");
 const PluginsController = require('../../Controllers/Plugins/PluginsController')
 const AddLogs = require('../../Functions/AddLogs')
+const {loadAllPlugins} = require('../../Functions/LoadPlugin')
 
 router.get("/", Auth, VerifyPermissions("canManagePlugins"), PluginsController.getPluginsInstalled)
 router.get("/installable", Auth, VerifyPermissions("canManagePlugins"), PluginsController.getInstallablePlugins)
-router.post("/install/:id", Auth, VerifyPermissions("canManagePlugins"),AddLogs("Install a new plugin" , "green"), PluginsController.InstallPlugin)
+router.post("/install/:id", Auth, VerifyPermissions("canManagePlugins"),AddLogs("Install a new plugin" , "green"), (req,res) => PluginsController.InstallPlugin(req,res,router))
 router.get("/user-routes", PluginsController.getUserRoutes)
+
+loadAllPlugins(router)
+
 
 
 
