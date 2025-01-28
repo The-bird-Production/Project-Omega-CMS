9e7540f3b39c96c1ca2594b1e4a0074dd717c271
Faynix <59486270+Faynix-code@users.noreply.github.com>
2025-01-28 20:41:58 +0100

    Create LoadPlugin function
    Work with the router of plugin in parameter

---
+++ Server/Functions/LoadPlugin.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+const path = require("path");
+
+// Dossier contenant les plugins
+const PLUGIN_DIR = path.join(process.cwd(), "Plugins");
+
+// Fonction pour charger un plugin spécifique
+const loadPlugin = (app, pluginId) => {
+  const pluginDir = path.join(PLUGIN_DIR, pluginId);
+  const pluginJsonPath = path.join(pluginDir, "plugin.json");
+
+  if (fs.existsSync(pluginJsonPath)) {
+    const pluginData = JSON.parse(fs.readFileSync(pluginJsonPath, "utf-8"));
+
+    const { id, url } = pluginData;
+
+    try {
+      // Charger les routes du plugin
+      const pluginRoutesPath = path.join(pluginDir, "Routes", "MainRoutes.js");
+      if (fs.existsSync(pluginRoutesPath)) {
+        const pluginRoutes = require(pluginRoutesPath);
+
+        console.log(pluginRoutes);
+        app.use(url || `/${url}`, pluginRoutes); // Utiliser l'URL spécifiée dans le plugin.json
+
+        console.log(`Plugin chargé : ${id} avec l'URL ${url || `/${url}`}`);
+      } else {
+        console.warn(`Le plugin "${id}" n'a pas de fichier routes.js.`);
+      }
+    } catch (error) {
+      console.error(`Erreur lors du chargement du plugin "${id}" :`, error);
+    }
+  } else {
+    console.warn(`Le plugin "${pluginName}" n'a pas de fichier plugin.json.`);
+  }
+};
+
+// Fonction pour charger tous les plugins présents dans le dossier au démarrage
+const loadAllPlugins = (app) => {
+  const pluginDirs = fs.readdirSync(PLUGIN_DIR);
+
+  pluginDirs.forEach((pluginDir) => {
+    const pluginJsonPath = path.join(PLUGIN_DIR, pluginDir, "plugin.json");
+
+    if (fs.existsSync(pluginJsonPath)) {
+      const data = JSON.parse(fs.readFileSync(pluginJsonPath, "utf-8"));
+      const { id } = data;
+      loadPlugin(app, id);
+    }
+  });
+};
+
+module.exports = {
+  loadPlugin,
+  loadAllPlugins,
+};
