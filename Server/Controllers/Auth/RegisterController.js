d74aa3d3cbebdf49f959add979f3f2db23cc6cb5
Faynix <59486270+Faynix-code@users.noreply.github.com>
2025-03-02 19:36:48 +0100

    Change default role

--- Server/Controllers/Auth/RegisterController.js
+++ Server/Controllers/Auth/RegisterController.js
@@ -16,7 +16,7 @@ exports.Register = async (req, res) => {
         username: req.body.username,
         email: req.body.email,
         password: await hash(req.body.password),
-        roleId: 21
+        roleId: 1
       },
     });
 
