c7d228d5a3e168426276928772e1f8ea40ecea1e
Faynix <59486270+Faynix-code@users.noreply.github.com>
2025-03-02 19:21:10 +0100

    Add detectOpenHandles option due to server

--- Server/jest.config.js
+++ Server/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     setupFilesAfterEnv: ["<rootDir>/tests/jest.setup.js"],
-    testMatch: ["**/tests/**/*.test.js"]
+    testMatch: ["**/tests/**/*.test.js"],
+    detectOpenHandles: true,
   };
   
