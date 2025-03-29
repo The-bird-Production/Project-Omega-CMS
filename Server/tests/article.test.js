24bd8733340113affd94b2ee8e1cf196e4525e40
Faynix <59486270+Faynix-code@users.noreply.github.com>
2025-03-29 17:51:02 +0100

    Correct bug in variable naming

--- Server/tests/article.test.js
+++ Server/tests/article.test.js
@@ -19,19 +19,19 @@ describe("Article API", () => {
         title: "test",
         slug: "slug",
         body: "Test",
+        authorId: 1,
         publishedAt: new Date("2025-01-01"),
       });
 
     expect(res.statusCode).toBe(201);
 
-    const user = await prisma.article.findUnique({
+    const article = await prisma.article.findUnique({
       where: { slug: "slug" },
     });
-    expect(user).not.toBeNull();
+    expect(article).not.toBeNull();
   });
   it("devrait récupérer un article", async () => {
     const res = await request(app).get("/article/slug/");
-
     expect(res.statusCode).toBe(200);
   });
   it("devrait récupérer tous les articles", async () => {
@@ -40,12 +40,12 @@ describe("Article API", () => {
   });
   it("devrait mettre à jour un article", async () => {
     const res = await request(app)
-      .put("/article/update/")
+      .put("/article/update/slug")
       .type("form")
       .send({
         title: "test",
         slug: "slug",
-        body: "Test",
+        content: "Test",
         publishedAt: new Date("2025-01-01"),
       });
     expect(res.statusCode).toBe(200);
