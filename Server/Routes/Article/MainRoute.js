e9a08aa5f5f1d7a06383da3234ba3e5d26dd4852
Faynix <59486270+Faynix-code@users.noreply.github.com>
2025-03-29 17:51:21 +0100

    Change route order and correct permissions bug

--- Server/Routes/Article/MainRoute.js
+++ Server/Routes/Article/MainRoute.js
@@ -5,13 +5,12 @@ const router = express.Router();
 const Auth = require('../../Middleware/Auth')
 const VerifiyPermissions = require('../../Middleware/VerifyPermissions')
 
-
+router.get('/all', require('../../Controllers/Article/ArticleController').getAllArticles);
 router.post('/create', Auth, VerifiyPermissions("canManageArticle"), require('../../Controllers/Article/ArticleController').createArticle);
-router.put('/update/:id', Auth, VerifiyPermissions("canManageArticle"), require('../../Controllers/Article/ArticleController').modifyArticle);
-router.get('/all', Auth, VerifiyPermissions, require('../../Controllers/Article/ArticleController').getAllArticles);
-router.get('/:slug', Auth, VerifiyPermissions, require('../../Controllers/Article/ArticleController').getArticleBySlug);
-router.delete('/delete/:id', Auth, VerifiyPermissions("canManageArticle"), require('../../Controllers/Article/ArticleController').deleteArticle);
+router.put('/update/:slug', Auth, VerifiyPermissions("canManageArticle"), require('../../Controllers/Article/ArticleController').modifyArticle);
+router.delete('/delete/:slug', Auth, VerifiyPermissions("canManageArticle"), require('../../Controllers/Article/ArticleController').deleteArticle);
 router.post('/save/:id', Auth, VerifiyPermissions("canManageArticle"), require('../../Controllers/Article/ArticleController').saveArticle);
+router.get('/:slug', require('../../Controllers/Article/ArticleController').getArticleBySlug);
 
 
 
