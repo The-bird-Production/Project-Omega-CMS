26290c3b7d0052e2649090a8b0e2804e320d3f57
Faynix <59486270+Faynix-code@users.noreply.github.com>
2025-10-04 13:28:09 +0200

    Convert jest setup to esm

--- Server/tests/jest.setup.js
+++ Server/tests/jest.setup.js
@@ -1,9 +1,9 @@
 //Convert to esm 
 import { configDotenv } from "dotenv";
 import { execSync } from "child_process";
-configDotenv({path: ".env.test"});
+configDotenv({ path: ".env.test" });
 
-module.exports = async () => {
+export default async function setup() {
   console.log("🔄 Reset de la base de données de test...");
   execSync("npm run test:setup", { stdio: "inherit" });
-};
+}
